d97dc649fbb79542dad5a17710b324f8bddd82fb
malak <gachimalak233@gmail.com>
2026-05-01 20:21:18 +0100

    last

---
+++ test/script.js
@@ -0,0 +1,16 @@
+function fibonacci(n) {
+  let result = [];
+
+  let a = 0,
+    b = 1;
+
+  for (let i = 0; i < n; i++) {
+    result.push(a);
+
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+
+  return result;
+}
